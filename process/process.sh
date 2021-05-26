ead292de889fe38bc9f8b7ba2cccf80ba909dcfe
C Spencer Jones <csj2114@ruth.cm.cluster>
2021-05-26 13:11:27 -0400

    ginsburg postprocess single slice

---
+++ process/process.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+
+source /burg/opt/anaconda3-2020.11/anaconda3/bin/activate ginsnburg_env
+
+python -c'import func_process; func_process.flt_process(6048,"/burg/abernathey/users/csj2114/agulhas-offline/time_1")'
