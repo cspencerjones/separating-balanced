15d1294d1e88511cd3992c76fa801b7b3c32bff8
C. Spencer Jones <csj2114@ruth.cm.cluster>
2022-05-06 16:48:07 -0400

    fixed filter width bug

--- time_75days/execut_filter.sh
+++ time_75days/execut_filter.sh
@@ -5,4 +5,4 @@
 source activate ginsnburg_env
 
 python -c'import py_filter; py_filter.auto_filter(
-"/burg/abernathey/users/csj2114/agulhas-offline/time_75days/",51)'
+"/burg/abernathey/users/csj2114/agulhas-offline/time_75days/",72)'
